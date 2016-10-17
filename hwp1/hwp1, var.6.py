e551a71d658fbca6e5f14177eaa34d2571461b7b
lerunok <maxler1277@yandex.ru>
2016-10-17 11:02:08 +0300

    hwp1

---
+++ hwp1/hwp1, var.6.py
@@ -0,0 +1,11 @@
+x=int(input('A: '))
+y=int(input('B: '))
+z=int(input('C: '))
+if x%y==z:
+    print('a даёт остаток c при делении на b')
+else:
+    print('a НЕ даёт остаток c при делении на b')
+if x/y==z:
+    print('a разделить на b равно c')
+else:
+    print('a разделить на b НЕ равно c')
